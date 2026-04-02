72ac70e79f8fd16cc0f4f2d903185e620f069dd1
ET24DE17 <andreagustingarci05@gmail.com>
2026-04-01 21:59:11 -0300

    implementacio

--- Trabajo Dylan Andres/Trabajo Dylan Andres/Form1.Designer.cs
+++ Trabajo Dylan Andres/Trabajo Dylan Andres/Form1.Designer.cs
@@ -28,12 +28,274 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtdni = new TextBox();
+            txtnombre = new TextBox();
+            label4 = new Label();
+            txtdnialquilar = new TextBox();
+            label5 = new Label();
+            txtcodigo = new TextBox();
+            label6 = new Label();
+            button2 = new Button();
+            label9 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            label7 = new Label();
+            txtnamepeli = new TextBox();
+            label8 = new Label();
+            label10 = new Label();
+            txtprecio = new TextBox();
+            txtdias = new TextBox();
+            label11 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(88, 174);
+            button1.Name = "button1";
+            button1.Size = new Size(130, 57);
+            button1.TabIndex = 0;
+            button1.Text = "Agregar a la base de datos";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(100, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Agregar cliente";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 15);
+            label2.TabIndex = 2;
+            label2.Text = "DNI";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(22, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Nombre";
+            // 
+            // txtdni
+            // 
+            txtdni.Location = new Point(88, 91);
+            txtdni.Name = "txtdni";
+            txtdni.Size = new Size(133, 23);
+            txtdni.TabIndex = 4;
+            // 
+            // txtnombre
+            // 
+            txtnombre.Location = new Point(88, 128);
+            txtnombre.Name = "txtnombre";
+            txtnombre.Size = new Size(133, 23);
+            txtnombre.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(615, 60);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Alquilar pelicula";
+            // 
+            // txtdnialquilar
+            // 
+            txtdnialquilar.Location = new Point(615, 99);
+            txtdnialquilar.Name = "txtdnialquilar";
+            txtdnialquilar.Size = new Size(133, 23);
+            txtdnialquilar.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(503, 104);
+            label5.Name = "label5";
+            label5.Size = new Size(27, 15);
+            label5.TabIndex = 7;
+            label5.Text = "DNI";
+            // 
+            // txtcodigo
+            // 
+            txtcodigo.Location = new Point(615, 138);
+            txtcodigo.Name = "txtcodigo";
+            txtcodigo.Size = new Size(133, 23);
+            txtcodigo.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(503, 146);
+            label6.Name = "label6";
+            label6.Size = new Size(106, 15);
+            label6.TabIndex = 9;
+            label6.Text = "Codigo de pelicula";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(615, 212);
+            button2.Name = "button2";
+            button2.Size = new Size(130, 57);
+            button2.TabIndex = 11;
+            button2.Text = "Alquilar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(338, 295);
+            label9.Name = "label9";
+            label9.Size = new Size(136, 15);
+            label9.TabIndex = 12;
+            label9.Text = "Generar reporte semanal";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(344, 339);
+            button3.Name = "button3";
+            button3.Size = new Size(130, 57);
+            button3.TabIndex = 13;
+            button3.Text = "Generar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(312, 174);
+            button4.Name = "button4";
+            button4.Size = new Size(129, 57);
+            button4.TabIndex = 14;
+            button4.Text = "Agregar al inventario";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(328, 50);
+            label7.Name = "label7";
+            label7.Size = new Size(88, 15);
+            label7.TabIndex = 15;
+            label7.Text = "Alta de pelicula";
+            // 
+            // txtnamepeli
+            // 
+            txtnamepeli.Location = new Point(319, 91);
+            txtnamepeli.Name = "txtnamepeli";
+            txtnamepeli.Size = new Size(135, 23);
+            txtnamepeli.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(262, 94);
+            label8.Name = "label8";
+            label8.Size = new Size(51, 15);
+            label8.TabIndex = 17;
+            label8.Text = "Nombre";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(262, 131);
+            label10.Name = "label10";
+            label10.Size = new Size(40, 15);
+            label10.TabIndex = 18;
+            label10.Text = "Precio";
+            // 
+            // txtprecio
+            // 
+            txtprecio.Location = new Point(312, 131);
+            txtprecio.Name = "txtprecio";
+            txtprecio.Size = new Size(135, 23);
+            txtprecio.TabIndex = 19;
+            // 
+            // txtdias
+            // 
+            txtdias.Location = new Point(615, 174);
+            txtdias.Name = "txtdias";
+            txtdias.Size = new Size(133, 23);
+            txtdias.TabIndex = 21;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(503, 177);
+            label11.Name = "label11";
+            label11.Size = new Size(29, 15);
+            label11.TabIndex = 20;
+            label11.Text = "Dias";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtdias);
+            Controls.Add(label11);
+            Controls.Add(txtprecio);
+            Controls.Add(label10);
+            Controls.Add(label8);
+            Controls.Add(txtnamepeli);
+            Controls.Add(label7);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(label9);
+            Controls.Add(button2);
+            Controls.Add(txtcodigo);
+            Controls.Add(label6);
+            Controls.Add(txtdnialquilar);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(txtnombre);
+            Controls.Add(txtdni);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtdni;
+        private TextBox txtnombre;
+        private Label label4;
+        private TextBox txtdnialquilar;
+        private Label label5;
+        private TextBox txtcodigo;
+        private Label label6;
+        private Button button2;
+        private Label label9;
+        private Button button3;
+        private Button button4;
+        private Label label7;
+        private TextBox txtnamepeli;
+        private Label label8;
+        private Label label10;
+        private TextBox txtprecio;
+        private TextBox txtdias;
+        private Label label11;
     }
 }
--- Trabajo Dylan Andres/Trabajo Dylan Andres/Form1.cs
+++ Trabajo Dylan Andres/Trabajo Dylan Andres/Form1.cs
@@ -1,10 +1,105 @@
 namespace Trabajo_Dylan_Andres
 {
+    using System.IO;
+    using System.Diagnostics;
     public partial class Form1 : Form
     {
+        int dni;
+        string nombre;
+        string nombrepelicula;
+        int precio;
+        int dnialquilar;
+        int codigodepelicula;
+        int dias;
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // aca dylan agregar a la base de datos, los nombres de los txtbox son txtdni y txtnombre
+            MessageBox.Show("Usuario registrado con extito!", "aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            dni = int.Parse(txtdni.Text);
+            nombre = txtnombre.Text;
+
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //primero tiene que verificar si el cliente posee alguna deuda
+            //, los nombres de los txt son txtdnialquilar y txtcodigo
+            MessageBox.Show("Pelicula Alquilada, que la disfrute!", "aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            nombrepelicula = txtnamepeli.Text;
+            precio = int.Parse(txtprecio.Text);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dnialquilar = int.Parse(txtdnialquilar.Text);
+            codigodepelicula = int.Parse(txtcodigo.Text);
+            dias = int.Parse(txtdias.Text);
+
+
+            // 1. Definimos dónde se va a guardar el archivo (en este caso, en el Escritorio)
+            string rutaEscritorio = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string rutaArchivo = Path.Combine(rutaEscritorio, "Reporte_Semanal.txt");
+
+            // 2. Empezamos a armar el texto del reporte
+            string contenidoReporte = "========================================\n";
+            contenidoReporte += "      REPORTE SEMANAL DE ALQUILERES     \n";
+            contenidoReporte += "========================================\n";
+            contenidoReporte += $"Fecha de emisión: {DateTime.Now.ToString("dd/MM/yyyy HH:mm")}\n\n";
+
+            // -------------------------------------------------------------------------
+            // ZONA DE BASE DE DATOS (PARA CUANDO TU COMPAÑERO TERMINE)
+            // -------------------------------------------------------------------------
+            /*
+             * Aquí es donde tendrán que conectar a la base de datos. El código será 
+             * algo parecido a esto (dependiendo de si usan SQL Server, MySQL, etc.):
+             * * SqlConnection conexion = new SqlConnection("TU_CADENA_DE_CONEXION_AQUI");
+             * conexion.Open();
+             * * // Hacen una consulta para traer lo de la última semana
+             * string query = "SELECT NombrePelicula, DniCliente, Fecha FROM Alquileres WHERE Fecha >= FechaInicioSemana";
+             * SqlCommand comando = new SqlCommand(query, conexion);
+             * SqlDataReader lector = comando.ExecuteReader();
+             * * while (lector.Read()) 
+             * {
+             * // Por cada fila que encuentren, la suman al texto:
+             * contenidoReporte += $"- Película: {lector["NombrePelicula"]} | DNI: {lector["DniCliente"]}\n";
+             * }
+             * * conexion.Close();
+             */
+            // -------------------------------------------------------------------------
+
+
+            // 3. DATOS SIMULADOS (Mietras tanto, usamos esto para probar que el .txt funciona)
+            contenidoReporte += ">> DATOS DE PRUEBA (Falta conectar BD):\n\n";
+            contenidoReporte += "- Película: El Señor de los Anillos | DNI: 12345678 | Fecha: 01/04/2026\n";
+            contenidoReporte += "- Película: Matrix                  | DNI: 87654321 | Fecha: 02/04/2026\n";
+            contenidoReporte += "- Película: Shrek 2                 | DNI: 11223344 | Fecha: 03/04/2026\n";
+            contenidoReporte += "\n========================================\n";
+            contenidoReporte += "Fin del reporte.";
+
+            try
+            {
+                // 4. Creamos el archivo de texto y le metemos todo el string que armamos
+                File.WriteAllText(rutaArchivo, contenidoReporte);
+
+                // 5. Abrimos el bloc de notas apuntando a ese archivo
+                Process.Start("notepad.exe", rutaArchivo);
+            }
+            catch (Exception ex)
+            {
+                // Por si llega a haber un error (ej. permisos de Windows)
+                MessageBox.Show("Hubo un error al generar el reporte: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pelicula Agregada con exito!", "aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
